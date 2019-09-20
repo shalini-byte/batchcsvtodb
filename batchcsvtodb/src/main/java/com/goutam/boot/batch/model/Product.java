4696706cb74f2f252ef077a93829e415785aaeef
Shalini Goutam <shalini22goutam@gmail.com>
2019-09-20 14:50:29 +0530

    Initial Commit

---
+++ batchcsvtodb/src/main/java/com/goutam/boot/batch/model/Product.java
@@ -0,0 +1,17 @@
+package com.goutam.boot.batch.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+	
+	private Integer id;
+	private String name;
+	private String description;
+	private Double price;
+	
+	
+
+}
